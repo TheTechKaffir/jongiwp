856643922ed7d45b8c2d3807656fbbbe3553e791
TheTechKaffir <jongim@jongibrandz.co.za>
2023-09-18 20:50:08 +0200

    Added a header and access control code

---
+++ wp-content/plugins/jongi-contact-form/jongi-contact-form.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+ * Plugin Name:       Jongi Contact Form
+ * Plugin URI:        https://jongibrandz.co.za/plugins/jongi-contact-form/
+ * Description:       Simple display of Contact Forms in the Frontend, including record listing in the WP Admin Portal.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Jongi - The Tech Kaffir
+ * Author URI:        https://jongimbodla.africa/
+ * Text Domain:       jongi-contact-form
+ */
+
+if(!defined('ABSPATH'))
+{
+    die("Ndikubhaqile uzam'ukungena ngondlela mnyama!!");
+}
